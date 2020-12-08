75df0017e2edfe132fd68728d23fd45bfb0dce0b
Alex McAuliffe <alex.mcauliffe@willistowers.com>
2020-12-08 09:25:09 +0000

    Day 8 - Handheld Halting

---
+++ src/years/2020/day-eight/Challenge.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text.RegularExpressions;
+using NodaTime;
+using Shared;
+
+namespace DayEight2020
+{
+    public class Challenge : Shared.Challenge
+    {
+        public override ChallengeInfo Info { get; } = new ChallengeInfo(new LocalDate(2020, 12, 08), "Handheld Halting");
+
+        public override void PartOne(IInput input, IOutput output)
+        {
+            var instructions = input.ParseInstructions();
+            var (accumulator, _) = Computer.RunInstructions(instructions);
+
+            output.WriteProperty("Accumulator before loop", accumulator);
+        }
+
+        public override void PartTwo(IInput input, IOutput output)
+        {
+            var instructions = input.ParseInstructions();
+            var targetQueue = new Queue<int>(Enumerable.Range(0, instructions.Length).Where(i =>
+            {
+                return instructions[i].Type is "nop" or "jmp";
+            }));
+
+            int? finalAccumulator = null;
+            while (targetQueue.TryDequeue(out var index))
+            {
+                var candidate = instructions[index];
+                var replacement = candidate switch
+                {
+                    { Type: "nop" } => candidate with { Type = "jmp" },
+                    { Type: "jmp" } => candidate with { Type = "nop" },
+                    _ => throw new InvalidOperationException()
+                };
+
+                var altered = instructions.Replace(candidate, replacement, ReferenceEqualityComparer.Instance);
+                var (accumulator, finished) = Computer.RunInstructions(altered);
+
+                if (finished)
+                {
+                    finalAccumulator = accumulator;
+                    break;
+                }
+            }
+
+            if (finalAccumulator.HasValue is false)
+            {
+                throw new InvalidOperationException("Tried everything captain!!");
+            }
+
+            output.WriteProperty("Final Accumulator Value", finalAccumulator!.Value);
+        }
+    }
+
+    public static class ParsingExtensions
+    {
+        private static readonly Regex instructionRegex = new Regex("(.*) ([+-]+\\d*)");
+
+        public static ImmutableArray<Instruction> ParseInstructions(this IInput input)
+        {
+            var builder = ImmutableArray.CreateBuilder<Instruction>();
+            foreach (var line in input.AsLines())
+            {
+                var match = instructionRegex.Match(line.ToString());
+
+                builder.Add(new Instruction(match.Groups[1].Value, int.Parse(match.Groups[2].Value)));
+            }
+
+            return builder.ToImmutable();
+        }
+    }
+}
---
+++ src/years/2020/day-eight/Computer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+
+namespace DayEight2020
+{
+    public record Instruction(string Type, int Argument);
+
+    internal static class Computer
+    {
+        public static (int Accumulator, bool FinishedSuccessfully) RunInstructions(ImmutableArray<Instruction> instructions)
+        {
+            var hashset = new HashSet<Instruction>(ReferenceEqualityComparer.Instance);
+            var accumulator = 0;
+            var pointer = 0;
+            while (true)
+            {
+                if (pointer == instructions.Length)
+                {
+                    break;
+                }
+
+                var instruction = instructions[pointer];
+                if (hashset.Contains(instruction))
+                {
+                    return (accumulator, false);
+                }
+
+                hashset.Add(instruction);
+                (accumulator, pointer) = instruction switch
+                {
+                    { Type: "nop" } => (accumulator, pointer + 1),
+                    { Type: "acc" } => (accumulator + instruction.Argument, pointer + 1),
+                    { Type: "jmp" } => (accumulator, pointer + instruction.Argument),
+                    _ => throw new InvalidOperationException($"Invalid Instruction {instruction.Type}")
+                };
+            };
+
+            return (accumulator, true);
+        }
+    }
+}
