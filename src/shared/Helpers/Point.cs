162ad9b4abe844e3a79eb84d1ec114e17189bc62
Alex McAuliffe <alex.mcauliffe@willistowers.com>
2020-12-12 10:58:31 +0000

    Day 12 - Rain Risk

--- src/shared/Helpers/Point.cs
+++ src/shared/Helpers/Point.cs
@@ -4,6 +4,12 @@ namespace Shared
 {
     public record Point(int Row, int Column) : IComparable<Point?>
     {
+        public int X => Row;
+
+        public int Y => Column;
+
+        public static Point Origin { get; } = new(0, 0);
+
         public int CompareTo(Point? other) =>
             other is null
                 ? -1
@@ -11,6 +17,9 @@ namespace Shared
 
         public static Point operator +(Point left, Point right)
             => new(left.Row + right.Row, left.Column + right.Column);
+
+        public static Point operator -(Point left, Point right)
+            => new(left.Row - right.Row, left.Column - right.Column);
 
         public static Point operator +(Point point, Direction direction) => direction.DirectionType switch
         {
@@ -24,6 +33,38 @@ namespace Shared
             DirectionType.West => point + (-1, 0),
             _ => point
         };
+
+        public static Point AddInDirection(Point point, Direction direction, int count)
+        {
+            return direction.DirectionType switch
+            {
+                DirectionType.North => point + (0, -count),
+                DirectionType.NorthEast => point + (count, -count),
+                DirectionType.NorthWest => point + (-count, -count),
+                DirectionType.East => point + (count, 0),
+                DirectionType.South => point + (0, count),
+                DirectionType.SouthEast => point + (count, count),
+                DirectionType.SouthWest => point + (-count, count),
+                DirectionType.West => point + (-count, 0),
+                _ => point
+            };
+        }
+
+        public Point RotateAroundPivot(Point pivot, int angleInDegrees)
+        {
+            var radians = angleInDegrees * MathF.PI / 180;
+
+            var s = MathF.Sin(radians);
+            var c = MathF.Cos(radians);
+
+            var translated = this - pivot;
+
+            // Rotate
+            var xnew = (int)MathF.Round(translated.X * c - translated.Y * s);
+            var ynew = (int)MathF.Round(translated.X * s + translated.Y * c);
+
+            return new Point(xnew + pivot.X, ynew + pivot.Y);
+        }
 
         public static implicit operator Point((int X, int Y) i) => new(i.X, i.Y);
     }
---
+++ src/years/2020/day-twelve/Challenge.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using NodaTime;
+using Shared;
+using Shared.Helpers;
+
+namespace DayTwelve2020
+{
+    public class Challenge : Shared.Challenge
+    {
+        public override ChallengeInfo Info { get; } = new ChallengeInfo(new LocalDate(2020, 12, 12), "Rain Risk");
+
+        public override void PartOne(IInput input, IOutput output)
+        {
+            var ship = new Ship(Direction.East, Point.Origin);
+
+            foreach (var line in input.AsLines())
+            {
+                var span = line.Span;
+                var command = span[0];
+                var rest = span[1..];
+
+                ship = command switch
+                {
+                    'N' => ship with { Position = Point.AddInDirection(ship.Position, Direction.North, int.Parse(rest)) },
+                    'S' => ship with { Position = Point.AddInDirection(ship.Position, Direction.South, int.Parse(rest)) },
+                    'E' => ship with { Position = Point.AddInDirection(ship.Position, Direction.East, int.Parse(rest)) },
+                    'W' => ship with { Position = Point.AddInDirection(ship.Position, Direction.West, int.Parse(rest)) },
+                    'L' or 'R' => ship with { Facing = MoveByDegrees(ship.Facing, command, int.Parse(rest)) },
+                    'F' => ship with
+                    {
+                        Position = Point.AddInDirection(ship.Position, ship.Facing, int.Parse(rest)),
+                    },
+                    _ => throw new InvalidOperationException($"Command not recognised {command}"),
+                };
+            }
+
+            output.WriteProperty("Ship", ship);
+            output.WriteProperty("Manhattan Distance", PointHelpers.ManhattanDistance(Point.Origin, ship.Position));
+
+            static Direction MoveByDegrees(Direction current, char direction, int value)
+            {
+                var res = current;
+                var count = value / 90;
+                for (var i = 0; i < count; i++)
+                {
+                    if (direction == 'R')
+                    {
+                        res = res.Right();
+                    }
+                    else if (direction == 'L')
+                    {
+                        res = res.Left();
+                    }
+                }
+
+                return res;
+            }
+        }
+
+        public override void PartTwo(IInput input, IOutput output)
+        {
+            var waypoint = new Point(10, -1);
+            var ship = new Ship(Direction.East, Point.Origin);
+
+            foreach (var line in input.AsLines())
+            {
+                var span = line.Span;
+                var command = span[0];
+                var argument = int.Parse(span[1..]);
+
+                (ship, waypoint) = command switch
+                {
+                    'N' => (ship, Point.AddInDirection(waypoint, Direction.North, argument)),
+                    'S' => (ship, Point.AddInDirection(waypoint, Direction.South, argument)),
+                    'E' => (ship, Point.AddInDirection(waypoint, Direction.East, argument)),
+                    'W' => (ship, Point.AddInDirection(waypoint, Direction.West, argument)),
+                    'L' or 'R' => (ship, RotateWaypoint(waypoint, command, argument)),
+                    'F' => (MoveToNTimes(ship, waypoint, argument), waypoint),
+                    _ => throw new InvalidOperationException($"Command not recognised {command}"),
+                };
+            }
+
+            output.WriteProperty("Ship", ship);
+            output.WriteProperty("Manhattan Distance", PointHelpers.ManhattanDistance(Point.Origin, ship.Position));
+
+            static Point RotateWaypoint(Point waypoint, char command, int argument)
+            {
+                var adjustedAngle = command == 'L'
+                    ? -argument
+                    : argument;
+
+                return waypoint.RotateAroundPivot(Point.Origin, adjustedAngle);
+            }
+
+            static Ship MoveToNTimes(Ship ship, Point waypoint, int times)
+            {
+                var pos = ship.Position;
+                for (var i = 0; i < times; i++)
+                {
+                    pos += waypoint;
+                }
+
+                return ship with { Position = pos };
+            }
+        }
+    }
+
+    record Ship(Direction Facing, Point Position);
+}
