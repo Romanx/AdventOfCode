9340d32a4b3716d1824f5a14d5fd77c971df7b0c
Romanx <mcauliffe.alex@gmail.com>
2020-12-13 12:59:59 +0000

    Day 13 - Shuttle Search

---
+++ src/shared/Math/ChineseRemainderTheorem.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+
+namespace Shared.Math
+{
+    public static class ChineseRemainderTheorem
+    {
+        /// <summary>
+        /// https://brilliant.org/wiki/chinese-remainder-theorem/
+        /// </summary>
+        /// <param name="n"></param>
+        /// <param name="a"></param>
+        /// <returns></returns>
+        public static long Solve(long[] n, long[] a)
+        {
+            long prod = n.Aggregate(1L, (i, j) => i * j);
+            long p;
+            long sm = 0;
+            for (long i = 0; i < n.Length; i++)
+            {
+                p = prod / n[i];
+                sm += a[i] * ModularMultiplicativeInverse(p, n[i]) * p;
+            }
+            return sm % prod;
+
+            static long ModularMultiplicativeInverse(long a, long mod)
+            {
+                long b = a % mod;
+                for (long x = 1; x < mod; x++)
+                {
+                    if ((b * x) % mod == 1)
+                    {
+                        return x;
+                    }
+                }
+                return 1;
+            }
+        }
+    }
+}
---
+++ src/years/2020/day-thirteen/Challenge.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using NodaTime;
+using Shared;
+using Shared.Math;
+
+namespace DayThirteen2020
+{
+    public class Challenge : Shared.Challenge
+    {
+        public override ChallengeInfo Info { get; } = new ChallengeInfo(new LocalDate(2020, 12, 13), "Shuttle Search");
+
+        public override void PartOne(IInput input, IOutput output)
+        {
+            var (earliestTimestamp, busIds) = input.ParseInput();
+
+            var (busId, closestTime) = busIds
+                .Select(id =>
+                {
+                    var rounded = (int)Math.Truncate((decimal)earliestTimestamp / id);
+
+                    return (Id: id, ClosestTime: id * (rounded + 1));
+                })
+                .OrderBy(v => v.ClosestTime)
+                .First();
+
+            var timeWaiting = closestTime - earliestTimestamp;
+            output.WriteProperty("Closest Bus", busId);
+            output.WriteProperty("Closest Time", closestTime);
+            output.WriteProperty("Time Waiting", timeWaiting);
+            output.WriteProperty("Answer", timeWaiting * busId);
+        }
+
+        public override void PartTwo(IInput input, IOutput output)
+        {
+            var buses = input.ParseBuses();
+
+            // We decrement the offset since the remainder of the modulo should be BusId minus the offset.
+            var modulos = buses.Select(b => b.BusId - b.Offset).ToArray();
+            var numbers = buses.Select(b => b.BusId).ToArray();
+
+            var result = ChineseRemainderTheorem.Solve(numbers, modulos);
+            output.WriteProperty("Timestamp", result);
+        }
+    }
+
+    internal static class ParseExtensions
+    {
+        public static (int EarliestTimestamp, ImmutableArray<int> BusIds) ParseInput(this IInput input)
+        {
+            var lines = input.AsLines().ToArray();
+
+            int earliestTimestamp = int.Parse(lines[0].Span);
+
+            return (earliestTimestamp, ParseBusIds(lines[1].ToString()));
+
+            static ImmutableArray<int> ParseBusIds(string line)
+            {
+                return line
+                    .Split(",")
+                    .Where(l => l != "x")
+                    .Select(int.Parse)
+                    .ToImmutableArray();
+            }
+        }
+
+        public static ImmutableArray<(long BusId, long Offset)> ParseBuses(this IInput input)
+        {
+            var line = input.AsLines().Skip(1).First().ToString();
+
+            return line
+                .Split(",")
+                .Select((el, idx) => (el, idx))
+                .Where(x => x.el != "x")
+                .Select(x => (long.Parse(x.el), (long)x.idx))
+                .ToImmutableArray();
+        }
+    }
+}
