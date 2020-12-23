704d13a12f6c6a51aa993039cf7bcff162418321
Alex McAuliffe <alex.mcauliffe@willistowers.com>
2020-12-23 10:48:51 +0000

    Ignore source failures when restoring

--- template.ps1
+++ template.ps1
@@ -2,5 +2,5 @@ param (
     [string]$date = $(Get-Date -Format "yyyy-MM-dd")
 )
 
-dotnet run -p .\src\Template\Template.csproj -- $date
+dotnet run -p .\src\Template\Template.csproj --ignore-failed-sources -- $date
 gci src/years/*.csproj -R | Sort CreationTime -Descending | Select-Object -First 1 | Foreach-Object { dotnet sln add $_ }
