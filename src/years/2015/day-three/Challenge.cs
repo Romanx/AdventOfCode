6f295873540e2282fadd1832c44d15b423a9f17a
Alex McAuliffe <alex.mcauliffe@willistowers.com>
2021-08-26 15:16:43 +0100

    2015 Day 3: Perfectly Spherical Houses in a Vacuum

---
+++ src/years/2015/day-three/Challenge.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using NodaTime;
+using Shared;
+
+namespace DayThree2015
+{
+    public class Challenge : Shared.Challenge
+    {
+        public override ChallengeInfo Info { get; } = new ChallengeInfo(new LocalDate(2015, 12, 3), "Perfectly Spherical Houses in a Vacuum");
+
+        public override void PartOne(IInput input, IOutput output)
+        {
+            var position = Point2d.Origin;
+            var visisted = new HashSet<Point2d> { position };
+            foreach (var command in input.AsReadOnlyMemory().Span)
+            {
+                var direction = GetDirection(command);
+                position += direction;
+                visisted.Add(position);
+            }
+
+            output.WriteProperty("Visisted Count", visisted.Count);
+        }
+
+        public override void PartTwo(IInput input, IOutput output)
+        {
+            var visisted = new HashSet<Point2d> { Point2d.Origin };
+            var pawns = new[] { Point2d.Origin, Point2d.Origin };
+            var span = input.AsReadOnlyMemory().Span;
+            for (var i = 0; i < span.Length; i++)
+            {
+                var command = span[i];
+                var pawnIndex = i % pawns.Length;
+                var pawn = pawns[pawnIndex];
+
+                var direction = GetDirection(command);
+                pawns[pawnIndex] = pawn + direction;
+                visisted.Add(pawns[pawnIndex]);
+            }
+
+            output.WriteProperty("Visisted Count", visisted.Count);
+        }
+
+        public static Direction GetDirection(char command) => command switch
+        {
+            '^' => Direction.North,
+            '>' => Direction.East,
+            'v' => Direction.South,
+            '<' => Direction.West,
+            _ => throw new InvalidOperationException("Where?")
+        };
+    }
+}
