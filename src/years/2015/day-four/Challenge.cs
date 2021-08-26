4eed70480f75c8a9ba81954c6004ee49e7a984e3
Alex McAuliffe <alex.mcauliffe@willistowers.com>
2021-08-26 15:39:30 +0100

    2015 Day 4: The Ideal Stocking Stuffer

---
+++ src/years/2015/day-four/Challenge.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using NodaTime;
+using Shared;
+
+namespace DayFour2015
+{
+    public class Challenge : Shared.Challenge
+    {
+        public override ChallengeInfo Info { get; } = new ChallengeInfo(new LocalDate(2015, 12, 4), "The Ideal Stocking Stuffer");
+
+        public override void PartOne(IInput input, IOutput output)
+        {
+            var key = input.AsString();
+
+            var (adventCoin, inputHash, outputHash) = CalculateAdventCoin(key, "00000");
+
+            output.WriteProperty("Hash Input", inputHash);
+            output.WriteProperty("Hash Output", outputHash);
+            output.WriteProperty("Advent Coin", adventCoin);
+        }
+
+        public override void PartTwo(IInput input, IOutput output)
+        {
+            var key = input.AsString();
+
+            var (adventCoin, inputHash, outputHash) = CalculateAdventCoin(key, "000000");
+
+            output.WriteProperty("Hash Input", inputHash);
+            output.WriteProperty("Hash Output", outputHash);
+            output.WriteProperty("Advent Coin", adventCoin);
+        }
+
+        private static (int AdventCoin, string HashInput, string HashOutput) CalculateAdventCoin(string key, string targetPrefix)
+        {
+            using var md5 = MD5.Create();
+            string hashInput;
+            string hashOutput;
+            int i;
+            for (i = 0; ; i++)
+            {
+                hashInput = $"{key}{i}";
+                var result = md5.ComputeHash(Encoding.ASCII.GetBytes(hashInput));
+
+                hashOutput = Convert.ToHexString(result);
+                if (hashOutput.StartsWith(targetPrefix))
+                {
+                    break;
+                }
+            }
+
+            return (i, hashInput, hashOutput);
+        }
+    }
+
+    internal static class ParseExtensions
+    {
+    }
+}
