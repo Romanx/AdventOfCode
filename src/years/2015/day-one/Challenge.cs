c32d5218de28650383099da31df22aad77ddb7aa
Alex McAuliffe <alex.mcauliffe@willistowers.com>
2021-08-26 14:15:26 +0100

    2015 Day 1: Not Quite Lisp

---
+++ src/years/2015/day-one/Challenge.cs
@@ -0,0 +1,52 @@
+﻿using NodaTime;
+using Shared;
+
+namespace DayOne2015
+{
+    public class Challenge : Shared.Challenge
+    {
+        public override ChallengeInfo Info { get; } = new ChallengeInfo(new LocalDate(2015, 12, 1), "Not Quite Lisp");
+
+        public override void PartOne(IInput input, IOutput output)
+        {
+            var floor = 0;
+            foreach (var c in input.AsReadOnlyMemory().Span)
+            {
+                if (c == '(')
+                {
+                    floor++;
+                }
+                else if (c == ')')
+                {
+                    floor--;
+                }
+            }
+
+            output.WriteProperty("Floor", floor);
+        }
+
+        public override void PartTwo(IInput input, IOutput output)
+        {
+            var floor = 0;
+            var span = input.AsReadOnlyMemory().Span;
+            int i;
+            for (i = 0; i < span.Length; i++)
+            {
+                var c = span[i];
+                if (c == '(')
+                {
+                    floor++;
+                }
+                else if (c == ')')
+                {
+                    floor--;
+                }
+
+                if (floor == -1)
+                    break;
+            }
+
+            output.WriteProperty("Position", i + 1);
+        }
+    }
+}
