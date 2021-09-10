17219e8c616dd616f97e7f4c61558040ffde6e52
Alex McAuliffe <alex.mcauliffe@willistowers.com>
2021-09-10 08:35:57 +0100

    2015 Day 12: JSAbacusFramework.io

---
+++ src/years/2015/day-twelve/Challenge.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text.Json;
+using NodaTime;
+using Shared;
+
+namespace DayTwelve2015
+{
+    public class Challenge : Shared.Challenge
+    {
+        public override ChallengeInfo Info { get; } = new ChallengeInfo(new LocalDate(2015, 12, 12), "JSAbacusFramework.io");
+
+        public override void PartOne(IInput input, IOutput output)
+        {
+            var document = input.Parse();
+
+            var numbers = 0;
+
+            Walk(document.RootElement, node =>
+            {
+                numbers += node.GetInt32();
+            });
+
+            output.WriteProperty("Sum", numbers);
+
+            static void Walk(JsonElement node, Action<JsonElement> action)
+            {
+                if (node.ValueKind is JsonValueKind.Array)
+                {
+                    foreach (var child in node.EnumerateArray())
+                    {
+                        Walk(child, action);
+                    }
+                }
+                else if (node.ValueKind == JsonValueKind.Object)
+                {
+                    foreach (var child in node.EnumerateObject())
+                    {
+                        Walk(child.Value, action);
+                    }
+                }
+                else if (node.ValueKind == JsonValueKind.Number)
+                {
+                    action(node);
+                }
+            }
+        }
+
+        public override void PartTwo(IInput input, IOutput output)
+        {
+            var document = input.Parse();
+
+            var numbers = 0;
+
+            Walk(document.RootElement, node =>
+            {
+                numbers += node.GetInt32();
+            });
+
+            output.WriteProperty("Sum", numbers);
+
+            static void Walk(JsonElement node, Action<JsonElement> action)
+            {
+                if (node.ValueKind is JsonValueKind.Array)
+                {
+                    foreach (var child in node.EnumerateArray())
+                    {
+                        Walk(child, action);
+                    }
+                }
+                else if (node.ValueKind == JsonValueKind.Object)
+                {
+                    foreach (var child in node.EnumerateObject())
+                    {
+                        if (child.Value.ValueKind == JsonValueKind.String &&
+                            child.Value.GetString() == "red")
+                        {
+                            return;
+                        }
+                    }
+
+                    foreach (var child in node.EnumerateObject())
+                    {
+                        Walk(child.Value, action);
+                    }
+                }
+                else if (node.ValueKind == JsonValueKind.Number)
+                {
+                    action(node);
+                }
+            }
+        }
+    }
+
+    internal static class ParseExtensions
+    {
+        public static JsonDocument Parse(this IInput input)
+            => JsonDocument.Parse(input.AsReadOnlyMemory());
+    }
+}
