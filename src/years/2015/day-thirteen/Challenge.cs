c2c63d45c3f37038a47fd016d5703e9a9f636c6f
Alex McAuliffe <alex.mcauliffe@willistowers.com>
2021-09-10 09:42:56 +0100

    2015 Day 13: Knights of the Dinner Table

---
+++ src/years/2015/day-thirteen/Challenge.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+using MoreLinq;
+using NodaTime;
+using Shared;
+
+namespace DayThirteen2015
+{
+    public class Challenge : Shared.Challenge
+    {
+        public override ChallengeInfo Info { get; } = new ChallengeInfo(new LocalDate(2015, 12, 13), "Knights of the Dinner Table");
+
+        public override void PartOne(IInput input, IOutput output)
+        {
+            var map = input.Parse();
+
+            var bestHappiness = map.Keys.Permutations()
+                .MaxBy(possible => CalculateTableHappiness(possible, map))
+                .First();
+
+            output.WriteProperty("Table", string.Join(", ", bestHappiness));
+            output.WriteProperty("Table Happiness", CalculateTableHappiness(bestHappiness, map));
+        }
+
+        public override void PartTwo(IInput input, IOutput output)
+        {
+            var map = input.Parse();
+            map = AddMe(map);
+
+            var bestHappiness = map.Keys.Permutations()
+                .MaxBy(possible => CalculateTableHappiness(possible, map))
+                .First();
+
+            output.WriteProperty("Table", string.Join(", ", bestHappiness));
+            output.WriteProperty("Table Happiness", CalculateTableHappiness(bestHappiness, map));
+
+            static ImmutableDictionary<string, ImmutableDictionary<string, int>> AddMe(ImmutableDictionary<string, ImmutableDictionary<string, int>> map)
+            {
+                var results = map.ToBuilder();
+
+                results.Add("Me", map.Keys
+                    .ToImmutableDictionary(k => k, _ => 0));
+
+                foreach (var (key, value) in map)
+                {
+                    var builder = value.ToBuilder();
+                    builder.Add("Me", 0);
+                    results[key] = builder.ToImmutable();
+                }
+
+                return results.ToImmutable();
+            }
+        }
+
+        static int CalculateTableHappiness(IList<string> table, ImmutableDictionary<string, ImmutableDictionary<string, int>> map)
+        {
+            var total = 0;
+            for (var i = 0; i < table.Count; i++)
+            {
+                var current = table[i];
+                var happiness = map[current];
+                var before = table[ActualIndex(i - 1, table)];
+                var after = table[ActualIndex(i + 1, table)];
+
+                total += happiness[before];
+                total += happiness[after];
+            }
+
+            return total;
+
+            static int ActualIndex(int absoluteIndex, IList<string> table)
+            {
+                if (absoluteIndex < 0)
+                {
+                    return table.Count + (absoluteIndex % table.Count);
+                }
+
+                return absoluteIndex % table.Count;
+            }
+        }
+    }
+}
---
+++ src/years/2015/day-thirteen/ParseExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Text.RegularExpressions;
+using Shared;
+
+namespace DayThirteen2015
+{
+    internal static class ParseExtensions
+    {
+        private static readonly Regex regex = new(@"(?<Person>.*) would (?<Direction>.*) (?<Amount>[0-9]*) happiness units by sitting next to (?<Other>.*)\.");
+
+        public static ImmutableDictionary<string, ImmutableDictionary<string, int>> Parse(this IInput input)
+        {
+            var builder = new Dictionary<string, Dictionary<string, int>>();
+            var people = ImmutableHashSet.CreateBuilder<string>();
+            foreach (var line in input.AsStringLines())
+            {
+                var match = regex.Match(line);
+
+                var personA = match.Groups["Person"].Value;
+                var personB = match.Groups["Other"].Value;
+                var direction = match.Groups["Direction"].Value;
+                var amount = int.Parse(match.Groups["Amount"].Value);
+
+                var happiness = direction switch
+                {
+                    "lose" => amount * -1,
+                    "gain" => amount,
+                    _ => throw new InvalidOperationException(),
+                };
+
+                if (builder.TryGetValue(personA, out var map) is false)
+                {
+                    builder[personA] = map = new Dictionary<string, int>();
+                };
+
+                map.Add(personB, happiness);
+            }
+
+            return builder.ToImmutableDictionary(
+                k => k.Key,
+                v => v.Value.ToImmutableDictionary());
+        }
+    }
+}
