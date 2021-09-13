3d6edf2858981d1ce816dfab8c827574c2aeb686
Alex McAuliffe <alex.mcauliffe@willistowers.com>
2021-09-13 10:00:27 +0100

    2015 Day 16: Aunt Sue

---
+++ src/years/2015/day-sixteen/Challenge.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using MoreLinq;
+using NodaTime;
+using Shared;
+
+namespace DaySixteen2015
+{
+    public class Challenge : Shared.Challenge
+    {
+        public override ChallengeInfo Info { get; } = new ChallengeInfo(new LocalDate(2015, 12, 16), "Aunt Sue");
+
+        private static readonly ImmutableDictionary<string, int> TargetProps = new Dictionary<string, int>
+        {
+            ["children"] = 3,
+            ["cats"] = 7,
+            ["samoyeds"] = 2,
+            ["pomeranians"] = 3,
+            ["akitas"] = 0,
+            ["vizslas"] = 0,
+            ["goldfish"] = 5,
+            ["trees"] = 3,
+            ["cars"] = 2,
+            ["perfumes"] = 1,
+        }.ToImmutableDictionary();
+
+        public override void PartOne(IInput input, IOutput output)
+        {
+            var sues = input.Parse();
+
+            var best = sues
+                .MaxBy(sue => Score(sue))
+                .First();
+
+            output.WriteProperty("Sue", best);
+
+            static int Score(Sue sue)
+            {
+                var score = 0;
+                foreach (var (name, value) in TargetProps)
+                {
+                    if (sue.Properties.TryGetValue(name, out var sueValue) && value == sueValue)
+                    {
+                        score++;
+                    }
+                }
+
+                return score;
+            }
+        }
+
+        public override void PartTwo(IInput input, IOutput output)
+        {
+            var sues = input.Parse();
+
+            var best = sues
+                .MaxBy(sue => Score(sue))
+                .First();
+
+            output.WriteProperty("Sue", best);
+
+            static int Score(Sue sue)
+            {
+                var score = 0;
+                foreach (var (name, value) in TargetProps)
+                {
+                    if (sue.Properties.TryGetValue(name, out var sueValue))
+                    {
+                        if ((name == "cats" || name == "trees"))
+                        {
+                            if (sueValue > value)
+                            {
+                                score++;
+                            }
+                        }
+                        else if ((name == "pomeranians" || name == "goldfish"))
+                        {
+                            if (sueValue < value)
+                            {
+                                score++;
+                            }
+                        }
+                        else if (value == sueValue)
+                        {
+                            score++;
+                        }
+                    }
+                }
+
+                return score;
+            }
+        }
+    }
+
+    record Sue(int Number, ImmutableDictionary<string, int> Properties)
+    {
+        public override string ToString()
+        {
+            var builder = new StringBuilder();
+            builder.AppendLine($"Sue {Number}");
+            foreach (var (name, value) in Properties)
+            {
+                builder.AppendLine($"  - {name}: {value}");
+            }
+
+            return builder.ToString().Trim();
+        }
+    }
+}
---
+++ src/years/2015/day-sixteen/ParseExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Shared;
+
+namespace DaySixteen2015
+{
+    internal static class ParseExtensions
+    {
+        private static readonly Regex regex = new("Sue (?<Number>[0-9]*): (?<Props>.*)");
+
+        public static ImmutableArray<Sue> Parse(this IInput input)
+        {
+            var builder = ImmutableArray.CreateBuilder<Sue>();
+
+            foreach (var line in input.AsStringLines())
+            {
+                var match = regex.Match(line);
+                var number = int.Parse(match.Groups["Number"].Value);
+
+                var props = match.Groups["Props"].Value
+                    .Split(',')
+                    .Select(i =>
+                    {
+                        var parts = i.Split(':', StringSplitOptions.TrimEntries);
+                        return KeyValuePair.Create(parts[0], int.Parse(parts[1]));
+                    })
+                    .ToImmutableDictionary();
+
+                builder.Add(new Sue(number, props));
+            }
+
+            return builder.ToImmutable();
+        }
+    }
+}
