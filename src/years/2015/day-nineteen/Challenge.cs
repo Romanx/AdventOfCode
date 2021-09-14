4cbd1150e6a7af84e230b60573795a445412e092
Alex McAuliffe <alex.mcauliffe@willistowers.com>
2021-09-14 08:31:44 +0100

    2015 Day 19: Medicine for Rudolph

---
+++ src/years/2015/day-nineteen/Challenge.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text.RegularExpressions;
+using NodaTime;
+using Shared;
+using Spectre.Console;
+
+namespace DayNineteen2015
+{
+    public class Challenge : Shared.Challenge
+    {
+        public override ChallengeInfo Info { get; } = new ChallengeInfo(new LocalDate(2015, 12, 19), "Medicine for Rudolph");
+
+        public override void PartOne(IInput input, IOutput output)
+        {
+            var (replacements, originalMolecule) = input.Parse();
+
+            var constructor = new MoleculeConstructor(replacements);
+            var generatedMolecules = constructor.BuildMolecules(originalMolecule);
+
+            output.WriteProperty("Number of distinct molescules", generatedMolecules.Count());
+        }
+
+        public override void PartTwo(IInput input, IOutput output)
+        {
+            var (replacements, target) = input.Parse();
+
+            var search = target;
+            var count = 0;
+            while (search != "e")
+            {
+                foreach (var (key, replacement) in replacements)
+                {
+                    if (search.Contains(replacement))
+                    {
+                        var regex = new Regex(replacement);
+                        search = regex.Replace(search, key, 1);
+                        count++;
+                    }
+                }
+            }
+
+            output.WriteProperty("Number of steps to 'e'", count);
+        }
+    }
+
+    public class MoleculeConstructor
+    {
+        private readonly ImmutableArray<(string Target, string Replacement)> _replacements;
+
+        public MoleculeConstructor(ImmutableArray<(string Target, string Replacement)> replacements)
+        {
+            _replacements = replacements;
+        }
+
+        public IEnumerable<string> BuildMolecules(string originalMolecule)
+        {
+            return _replacements
+                .SelectMany(transform => ApplyReplacement(
+                    transform.Target,
+                    transform.Replacement,
+                    originalMolecule))
+                .Distinct();
+        }
+
+        private static IEnumerable<string> ApplyReplacement(string target, string replacement, string molecule)
+        {
+            var indexes = new List<int>();
+            var span = molecule.AsSpan();
+
+            while (span.IsEmpty is false)
+            {
+                var index = span.LastIndexOf(target);
+                if (index == -1)
+                {
+                    break;
+                }
+                indexes.Add(index);
+                span = span[..index];
+            }
+
+            var length = molecule.Length - target.Length + replacement.Length;
+            foreach (var index in indexes)
+            {
+                var context = (
+                    TargetIndex: index,
+                    TargetLength: target.Length,
+                    Replacement: replacement,
+                    Molecule: molecule
+                );
+
+                yield return string.Create(length, context, static (span, state) =>
+                {
+                    var original = state.Molecule.AsSpan();
+                    var result = span;
+
+                    var start = original[0..state.TargetIndex];
+                    start.CopyTo(result);
+                    result = result[start.Length..];
+
+                    var replacement = state.Replacement.AsSpan();
+                    replacement.CopyTo(result);
+                    result = result[replacement.Length..];
+
+                    var end = original[(state.TargetIndex + state.TargetLength)..];
+                    end.CopyTo(result);
+                    result = result[end.Length..];
+                });
+            }
+        }
+    }
+}
---
+++ src/years/2015/day-nineteen/ParseExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Immutable;
+using Shared;
+using Spectre.Console;
+
+namespace DayNineteen2015
+{
+    internal static class ParseExtensions
+    {
+        public static (ImmutableArray<(string Target, string Replacement)> Replacements, string Molecule) Parse(this IInput input)
+        {
+            var paragraphs = input.AsParagraphs();
+
+            var replacements = ParseReplacements(paragraphs[0]);
+
+            return (replacements, paragraphs[1].Span[0].ToString());
+        }
+
+        private static ImmutableArray<(string Target, string Replacement)> ParseReplacements(ReadOnlyMemory<ReadOnlyMemory<char>> readOnlyMemory)
+        {
+            const string Separator = "=>";
+            var builder = ImmutableArray.CreateBuilder<(string, string)>();
+
+            foreach (var line in readOnlyMemory.Span)
+            {
+                var span = line.Span;
+                var index = span.IndexOf(Separator);
+                var key = line.Span[0..index].Trim();
+                var value = line.Span[(index + 2)..].Trim();
+
+                builder.Add((new string(key), new string(value)));
+            }
+
+            return builder.ToImmutable();
+        }
+    }
+}
