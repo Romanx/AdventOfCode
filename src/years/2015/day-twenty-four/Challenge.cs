eae5fa414652ed0fedbcf09f6cc94e6fb3e67d41
Alex McAuliffe <alex.mcauliffe@willistowers.com>
2021-09-24 09:04:37 +0100

    2015 Day 24: It Hangs in the Balance

---
+++ src/years/2015/day-twenty-four/Challenge.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using MoreLinq;
+using NodaTime;
+using Shared;
+using Spectre.Console;
+
+namespace DayTwentyFour2015
+{
+    public class Challenge : Shared.Challenge
+    {
+        public override ChallengeInfo Info { get; } = new ChallengeInfo(new LocalDate(2015, 12, 24), "It Hangs in the Balance");
+
+        public override void PartOne(IInput input, IOutput output)
+        {
+            var vals = input.Parse();
+
+            var group = FindSmallestFirstGroup(vals, 3);
+
+            output.WriteProperty("Smallest First Group", string.Join(" ", group));
+            output.WriteProperty("QE", Product(group));
+        }
+
+        public override void PartTwo(IInput input, IOutput output)
+        {
+            var vals = input.Parse();
+
+            var group = FindSmallestFirstGroup(vals, 4);
+
+            output.WriteProperty("Smallest First Group", string.Join(" ", group));
+            output.WriteProperty("QE", Product(group));
+        }
+
+        private static IList<int> FindSmallestFirstGroup(
+            IEnumerable<int> input,
+            int numberOfGroups)
+        {
+            var groupSize = input.Sum() / numberOfGroups;
+            var found = false;
+            var size = 1;
+
+            var group = Enumerable.Empty<IList<int>>();
+            while (found is false)
+            {
+                group = input.Subsets(size).Where(s => s.Sum() == groupSize);
+                found = group.Any();
+                size++;
+            }
+
+            return group
+                .MinBy(s => Product(s))
+                .First();
+        }
+
+        static long Product(IEnumerable<int> iter) => iter
+            .Select(i => (long)i)
+            .Aggregate((a, b) => a * b);
+    }
+
+    internal static class ParseExtensions
+    {
+        public static ImmutableArray<int> Parse(this IInput input)
+            => input.AsLinesToInt().ToImmutableArray();
+    }
+}
