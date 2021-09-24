a855a66960b57f3d72d6b5ac9869e242beed2805
Alex McAuliffe <alex.mcauliffe@willistowers.com>
2021-09-24 11:53:54 +0100

    2016 Day 2: Bathroom Security

---
+++ src/years/2016/day-two/Challenge.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Text;
+using NodaTime;
+using Shared;
+
+namespace DayTwo2016
+{
+    public class Challenge : Shared.Challenge
+    {
+        public override ChallengeInfo Info { get; } = new ChallengeInfo(new LocalDate(2016, 12, 2), "Bathroom Security");
+
+        public override void PartOne(IInput input, IOutput output)
+        {
+            var builder = new StringBuilder();
+            var keypad = new Keypad(new char[,]
+            {
+                { '1', '2', '3' },
+                { '4', '5', '6' },
+                { '7', '8', '9' },
+            });
+            var position = keypad.FiveKey;
+
+            foreach (var line in input.AsLines())
+            {
+                (var key, position) = FindKey(keypad, line.Span, position);
+
+                builder.Append(key);
+            }
+
+            output.WriteProperty("Code", builder.ToString());
+        }
+
+        public override void PartTwo(IInput input, IOutput output)
+        {
+            var builder = new StringBuilder();
+            var keypad = new Keypad(new char[,]
+            {
+                { ' ', ' ', '1', ' ', ' ' },
+                { ' ', '2', '3', '4', ' ' },
+                { '5', '6', '7', '8', '9' },
+                { ' ', 'A', 'B', 'C', ' ' },
+                { ' ', ' ', 'D', ' ', ' ' },
+            });
+            var position = keypad.FiveKey;
+
+            foreach (var line in input.AsLines())
+            {
+                (var key, position) = FindKey(keypad, line.Span, position);
+
+                builder.Append(key);
+            }
+
+            output.WriteProperty("Code", builder.ToString());
+        }
+
+        private static (char Key, Point2d position) FindKey(
+            Keypad keypad,
+            ReadOnlySpan<char> span,
+            Point2d position)
+        {
+            foreach (var c in span)
+            {
+                var next = c switch
+                {
+                    'U' => position + GridDirection.Up,
+                    'R' => position + GridDirection.Right,
+                    'D' => position + GridDirection.Down,
+                    'L' => position + GridDirection.Left,
+                    _ => throw new InvalidOperationException("Not a valid movement")
+                };
+
+                if (keypad.Valid(next))
+                {
+                    position = next;
+                }
+            }
+
+            return (keypad.Key(position), position);
+        }
+    }
+}
---
+++ src/years/2016/day-two/Keypad.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.Toolkit.HighPerformance;
+using Shared;
+
+namespace DayTwo2016
+{
+    class Keypad
+    {
+        private readonly ImmutableDictionary<Point2d, char> _keypad;
+
+        public Keypad(char[,] pad)
+        {
+            var span = pad.AsSpan2D();
+
+            var builder = ImmutableDictionary.CreateBuilder<Point2d, char>();
+            for (var y = 0; y < span.Height; y++)
+            {
+                var row = span.GetRowSpan(y);
+                for (var x = 0; x < row.Length; x++)
+                {
+                    if (row[x] != ' ')
+                    {
+                        builder.Add(new Point2d(x, y), row[x]);
+                    }
+                }
+            }
+
+            _keypad = builder.ToImmutable();
+            FiveKey = _keypad.Where(kvp => kvp.Value == '5').First().Key;
+        }
+
+        public bool Valid(Point2d point) => _keypad.ContainsKey(point);
+
+        public Point2d FiveKey { get; }
+
+        public char Key(Point2d point) => _keypad[point];
+    }
+}
