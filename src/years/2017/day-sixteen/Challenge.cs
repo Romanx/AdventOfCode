ef1ec5ed4a6a63a3c22885c6e5758178ec0ad298
Alex McAuliffe <alex.mcauliffe@willistowers.com>
2021-11-01 13:29:42 +0000

    2017 Day 16: Permutation Promenade

---
+++ src/years/2017/day-sixteen/Challenge.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using NodaTime;
+using Shared;
+
+namespace DaySixteen2017
+{
+    public class Challenge : Shared.Challenge
+    {
+        public override ChallengeInfo Info { get; } = new ChallengeInfo(new LocalDate(2017, 12, 16), "Permutation Promenade");
+
+        public override void PartOne(IInput input, IOutput output)
+        {
+            var initial = "abcdefghijklmnop";
+            var instructions = input.Content.ParseInstructions();
+
+            var result = Dance(initial, instructions);
+
+            output.WriteProperty("Before", initial);
+            output.WriteProperty("After", result);
+        }
+
+        public override void PartTwo(IInput input, IOutput output)
+        {
+            const int Target = 1_000_000_000;
+            var seen = new HashSet<string>();
+
+            var initial = "abcdefghijklmnop";
+            seen.Add(initial);
+            var instructions = input.Content.ParseInstructions();
+
+            int i;
+
+            for (i = 1; ; i++)
+            {
+                var result = Dance(initial, instructions);
+                if (seen.Add(result) is false)
+                {
+                    break;
+                }
+                initial = result;
+            }
+
+            var list = seen.ToList();
+            var end = i;
+
+            var remainder = Target % end;
+
+            var position = list[remainder];
+
+            output.WriteProperty($"After {Target} loops", position);
+        }
+
+        private static string Dance(string initial, ImmutableArray<Instruction> instructions)
+        {
+            var state = (initial, instructions);
+
+            return string.Create(initial.Length, state, static (span, state) =>
+            {
+                var (initial, instructions) = state;
+                initial.AsSpan().CopyTo(span);
+
+                foreach (var instruction in instructions)
+                {
+                    instruction.Apply(span);
+                }
+            });
+        }
+    }
+}
---
+++ src/years/2017/day-sixteen/ParseExtensions.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Linq;
+using PCRE;
+using Shared;
+using Shared.Parser;
+
+namespace DaySixteen2017
+{
+    internal static class ParseExtensions
+    {
+        private static readonly CommandParser<Instruction> parser = new CommandParser<Instruction>()
+            .AddType<Spin>()
+            .AddType<Exchange>()
+            .AddType<Partner>();
+
+        public static ImmutableArray<Instruction> ParseInstructions(this IInputContent content)
+        {
+            var transformed = content
+                .Transform(str => str.Split(',', StringSplitOptions.RemoveEmptyEntries & StringSplitOptions.TrimEntries))
+                .Select(str => str.AsMemory());
+
+            return parser.ParseCommands(transformed)
+                .ToImmutableArray();
+        }
+
+
+        [CommandRegex(@"s(\d+)")]
+        record Spin(int X) : Instruction("Spin")
+        {
+            public override void Apply(Span<char> span)
+            {
+                Span<char> scratch = stackalloc char[span.Length];
+                var end = span[^X..];
+                end.CopyTo(scratch);
+                span[..^X].CopyTo(scratch[X..]);
+
+                scratch.CopyTo(span);
+            }
+
+            public static Spin Build(ref PcreRefMatch.GroupList groups)
+            {
+                return new Spin(int.Parse(groups[1].Value));
+            }
+        }
+
+        [CommandRegex(@"x(\d+)\/(\d+)")]
+        record Exchange(int A, int B) : Instruction("Exchange")
+        {
+            public override void Apply(Span<char> span)
+            {
+                var aVal = span[A];
+                var bVal = span[B];
+
+                span[A] = bVal;
+                span[B] = aVal;
+            }
+
+            public static Exchange Build(ref PcreRefMatch.GroupList groups)
+            {
+                var x = int.Parse(groups[1].Value);
+                var y = int.Parse(groups[2].Value);
+
+                return new Exchange(x, y);
+            }
+        }
+
+        [CommandRegex(@"p([a-z])\/([a-z])")]
+        record Partner(char A, char B) : Instruction("Partner")
+        {
+            public override void Apply(Span<char> span)
+            {
+                var aIndex = span.IndexOf(A);
+                var bIndex = span.IndexOf(B);
+
+                span[aIndex] = B;
+                span[bIndex] = A;
+            }
+
+            public static Partner Build(ref PcreRefMatch.GroupList groups)
+            {
+                var x = groups[1].Value[0];
+                var y = groups[2].Value[0];
+
+                return new Partner(x, y);
+            }
+        }
+    }
+
+    abstract record Instruction(string Name)
+    {
+        public abstract void Apply(Span<char> span);
+    }
+}
