7d6893b0f9ed9aa46227547f68ff15629fe4b432
Alex McAuliffe <alex.mcauliffe@willistowers.com>
2021-12-01 09:15:53 +0000

    2021 Day 1: Sonar Sweep

---
+++ src/years/2021/day-one/Challenge.cs
@@ -0,0 +1,54 @@
+﻿namespace DayOne2021;
+using static MoreLinq.Extensions.WindowExtension;
+
+public class Challenge : ChallengeSync
+{
+    public override ChallengeInfo Info { get; } = new ChallengeInfo(new LocalDate(2021, 12, 1), "Sonar Sweep");
+
+    public override void PartOne(IInput input, IOutput output)
+    {
+        var distances = input.Lines.Ints();
+
+        var changes = distances
+            .Window(2)
+            .Select(x => CalculateChange(x[1], x[0]));
+
+        output.WriteProperty("Number of Increases", changes.Count(x => x is ChangeType.Increase));
+    }
+
+    public override void PartTwo(IInput input, IOutput output)
+    {
+        var distances = input.Lines.Ints();
+
+        var changes = distances
+            .Window(3)
+            .Select(x => x.Sum())
+            .Window(2)
+            .Select(x => CalculateChange(x[1], x[0]));
+
+        output.WriteProperty("Number of Increases", changes.Count(x => x is ChangeType.Increase));
+    }
+
+    private static ChangeType CalculateChange(int current, int previous)
+    {
+        if (current == previous)
+        {
+            return ChangeType.NoChange;
+        }
+        else if (current > previous)
+        {
+            return ChangeType.Increase;
+        }
+        else
+        {
+            return ChangeType.Decrease;
+        }
+    }
+}
+
+enum ChangeType
+{
+    NoChange,
+    Increase,
+    Decrease,
+}
