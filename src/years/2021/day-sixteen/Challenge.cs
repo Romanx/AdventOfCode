3507a4bc9fb848019fd93a997bf2149a1dad858b
Alex McAuliffe <alex.mcauliffe@willistowers.com>
2021-12-16 13:10:44 +0000

    2021 Day 16: Packet Decoder

---
+++ src/years/2021/day-sixteen/Challenge.cs
@@ -0,0 +1,115 @@
+﻿using System.Buffers.Binary;
+using System.Buffers.Text;
+using System.Runtime.InteropServices;
+
+namespace DaySixteen2021;
+
+public class Challenge : ChallengeSync
+{
+    public override ChallengeInfo Info { get; } = new ChallengeInfo(new LocalDate(2021, 12, 16), "Packet Decoder");
+
+    public override void PartOne(IInput input, IOutput output)
+    {
+        var str = input.Content.AsString().Trim();
+        var binary = ConvertHexToBinary(str);
+
+        var packet = PacketParser.ParsePacket(binary);
+        var version = CalculateTotalVersion(packet);
+
+        output.WriteProperty("Total Version", version);
+
+        static int CalculateTotalVersion(Packet packet)
+        {
+            if (packet is LiteralPacket lp)
+            {
+                return lp.Version;
+            }
+            else if (packet is OperatorPacket op)
+            {
+                var sum = op.Version;
+                foreach (var inner in op.InnerPackets)
+                {
+                    sum += CalculateTotalVersion(inner);
+                }
+                return sum;
+            }
+
+            throw new InvalidOperationException("What!?");
+        }
+    }
+
+    public override void PartTwo(IInput input, IOutput output)
+    {
+        var str = input.Content.AsString();
+        var binary = ConvertHexToBinary(str);
+
+        var packet = PacketParser.ParsePacket(binary);
+
+        var result = packet.Calculate();
+
+        output.WriteProperty("Result of Packet", result);
+    }
+
+    static ReadOnlyMemory<char> ConvertHexToBinary(ReadOnlySpan<char> input)
+    {
+        var arr = new char[input.Length * 4];
+        var span = arr.AsSpan();
+
+        for (var i = 0; i < input.Length; i++)
+        {
+            ReadOnlySpan<char> bits = input[i] switch
+            {
+                '0' => "0000",
+                '1' => "0001",
+                '2' => "0010",
+                '3' => "0011",
+                '4' => "0100",
+                '5' => "0101",
+                '6' => "0110",
+                '7' => "0111",
+                '8' => "1000",
+                '9' => "1001",
+                'A' => "1010",
+                'B' => "1011",
+                'C' => "1100",
+                'D' => "1101",
+                'E' => "1110",
+                'F' => "1111",
+                _ => throw new InvalidOperationException($"Not a Hexidecimal value: '{input[i]}'")
+            };
+
+            bits.CopyTo(span[..4]);
+            span = span[4..];
+        }
+
+        return arr;
+    }
+}
+
+abstract record Packet(int Version, int Type)
+{
+    public abstract long Calculate();
+}
+
+record LiteralPacket(int Version, int Type, long Value) : Packet(Version, Type)
+{
+    public override long Calculate() => Value;
+}
+
+record OperatorPacket(int Version, int Type, int LengthType, Packet[] InnerPackets) : Packet(Version, Type)
+{
+    public override long Calculate()
+    {
+        return Type switch
+        {
+            0 => InnerPackets.Sum(ip => ip.Calculate()),
+            1 => InnerPackets.Aggregate(1L, (acc, ip) => acc * ip.Calculate()),
+            2 => InnerPackets.Min(ip => ip.Calculate()),
+            3 => InnerPackets.Max(ip => ip.Calculate()),
+            5 => InnerPackets[0].Calculate() > InnerPackets[1].Calculate() ? 1 : 0,
+            6 => InnerPackets[0].Calculate() < InnerPackets[1].Calculate() ? 1 : 0,
+            7 => InnerPackets[0].Calculate() == InnerPackets[1].Calculate() ? 1 : 0,
+            _ => throw new NotImplementedException($"Not implemented that type yet: '{Type}'"),
+        };
+    }
+}
---
+++ src/years/2021/day-sixteen/PacketParser.cs
@@ -0,0 +1,124 @@
+﻿namespace DaySixteen2021;
+
+static class PacketParser
+{
+    public static Packet ParsePacket(ReadOnlyMemory<char> binary)
+    {
+        var (packet, remaining) = ParsePacketInner(binary);
+
+        return packet;
+    }
+
+    private static (Packet Packet, ReadOnlyMemory<char> Remaining) ParsePacketInner(ReadOnlyMemory<char> binary)
+    {
+        var span = binary.Span;
+        var version = Convert.ToInt32(new string(span[0..3]), 2);
+        var type = Convert.ToInt32(new string(span[3..6]), 2);
+        var rest = binary[6..];
+
+        if (type == 4)
+        {
+            var (packet, remaining) = ParseLiteralPacket(version, type, rest);
+            return (packet, remaining);
+        }
+        else
+        {
+            var lengthTypeId = rest.Span[0];
+            if (lengthTypeId == '0')
+            {
+                var packetLength = rest[1..16];
+                rest = rest[16..];
+                var length = Convert.ToInt32(new string(packetLength.Span), 2);
+                var subpackets = rest[..length];
+                rest = rest[length..];
+
+                var packet = ParseOperatorPacketTypeZero(version, type, subpackets);
+                return (packet, rest);
+            }
+            else
+            {
+                var packetLength = rest[1..12];
+                rest = rest[12..];
+                var numberOfPackets = Convert.ToInt32(new string(packetLength.Span), 2);
+
+                var (packet, remaining) = ParseOperatorPacketTypeOne(version, type, numberOfPackets, rest);
+                return (packet, remaining);
+            }
+        }
+
+        throw new InvalidOperationException("Not handled yet!");
+    }
+
+    static (LiteralPacket Packet, ReadOnlyMemory<char> Remaining) ParseLiteralPacket(int version, int type, ReadOnlyMemory<char> binary)
+    {
+        var length = 0;
+
+        var valueBits = new List<char>();
+        var span = binary.Span;
+
+        var foundLast = false;
+        var index = 0;
+        while (foundLast is false)
+        {
+            if (span[index] == '0')
+            {
+                foundLast = true;
+            }
+
+            var slice = span[(index + 1)..(index + 5)];
+            for (var i = 0; i < slice.Length; i++)
+            {
+                valueBits.Add(slice[i]);
+            }
+
+            index += 5;
+            length += 5;
+        }
+
+        var value = Convert.ToInt64(string.Join("", valueBits), 2);
+
+        //if (length % 4 != 0)
+        //{
+        //    length += (length % 4);
+        //}
+
+        var rest = binary[length..];
+
+        return (new LiteralPacket(version, type, value), rest);
+    }
+
+    static OperatorPacket ParseOperatorPacketTypeZero(
+        int version,
+        int type,
+        ReadOnlyMemory<char> subpackets)
+    {
+        var packets = new List<Packet>();
+        while (subpackets.Length > 0)
+        {
+            var (packet, remaining) = ParsePacketInner(subpackets);
+
+            packets.Add(packet);
+            subpackets = remaining;
+        }
+
+        return new OperatorPacket(version, type, 0, packets.ToArray());
+    }
+
+    static (OperatorPacket Packet, ReadOnlyMemory<char> Remaining) ParseOperatorPacketTypeOne(
+        int version,
+        int type,
+        int packetCount,
+        ReadOnlyMemory<char> binary)
+    {
+        var packets = new Packet[packetCount];
+        var body = binary;
+
+        for (var i = 0; i < packets.Length; i++)
+        {
+            (var packet, body) = ParsePacketInner(body);
+            packets[i] = packet;
+        }
+
+        return (new OperatorPacket(version, type, 1, packets), body);
+    }
+}
