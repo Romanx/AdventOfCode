78b81a6b73f7095d1a5a45b068150fcae528031a
Alex McAuliffe <alex.mcauliffe@willistowers.com>
2022-04-29 15:19:40 +0100

    Clean up some of the code for 2021 day twelve

--- src/years/2021/day-twelve/Challenge.cs
+++ src/years/2021/day-twelve/Challenge.cs
@@ -1,4 +1,8 @@
-﻿namespace DayTwelve2021;
+﻿using System.Runtime.InteropServices;
+using Microsoft.Collections.Extensions;
+using Microsoft.Toolkit.HighPerformance;
+
+namespace DayTwelve2021;
 
 public class Challenge : ChallengeSync
 {
@@ -9,26 +13,25 @@ public class Challenge : ChallengeSync
         var graph = input.Lines.ParseGraph();
         var paths = FindPaths(
             graph,
-            "start",
-            "end",
             CaveFunction);
 
         output.WriteProperty("Number of paths", paths);
 
-        static bool CaveFunction(char cave, CaveRoute route)
+        static bool CaveFunction(Cave cave, CaveRoute route)
         {
-            return route.SmallCaveVisits.ContainsKey(cave) is false;
+            return route.SmallCaveVisits
+                .ContainsKey(cave) is false;
         }
     }
 
     public override void PartTwo(IInput input, IOutput output)
     {
         var graph = input.Lines.ParseGraph();
-        var paths = FindPaths(graph, "start", "end", CaveFunction);
+        var paths = FindPaths(graph, CaveFunction);
 
         output.WriteProperty("Number of paths", paths);
 
-        static bool CaveFunction(char cave, CaveRoute route)
+        static bool CaveFunction(Cave cave, CaveRoute route)
         {
             var visitedTwice = route.SmallCaveVisits.Any(kvp => kvp.Value is 2);
 
@@ -42,45 +45,47 @@ public class Challenge : ChallengeSync
         }
     }
 
-    int FindPaths(
-        Graph graph,
-        string start,
-        string target,
-        Func<char, CaveRoute, bool> caveFunc)
+    static int FindPaths(
+        ImmutableDictionary<string, Cave> graph,
+        Func<Cave, CaveRoute, bool> caveFunc)
     {
         var pathCount = 0;
 
+        var (_, start) = graph.First(g => g.Value.CaveType is CaveType.Entrance);
         var currentFrontier = new List<CaveRoute>();
         var nextFrontier = new List<CaveRoute>();
-        currentFrontier.Add(new CaveRoute(ImmutableArray.Create(start), ImmutableDictionary<char, int>.Empty));
+        currentFrontier.Add(new CaveRoute(ImmutableArray.Create(start), ImmutableDictionary<Cave, int>.Empty));
 
         while (currentFrontier.Count > 0)
         {
             foreach (var current in currentFrontier)
             {
-                foreach (var next in graph.GetEdges(current.Path[^1]))
+                var currentCave = current.Path[^1];
+
+                foreach (var next in currentCave.Connections)
                 {
+                    var nextCave = graph[next];
+
                     // If we're at the target then yay
-                    if (next == target)
+                    if (nextCave.CaveType is CaveType.Exit)
                     {
                         pathCount++;
                     }
                     // If it's the start we can't go back
-                    else if (next == start)
+                    else if (nextCave.CaveType is CaveType.Entrance)
                     {
                     }
                     // It's a small cave!
-                    else if (char.IsLower(next[0]))
+                    else if (nextCave.CaveType is CaveType.Small)
                     {
-                        var cave = next[0];
-                        if (caveFunc(cave, current))
+                        if (caveFunc(nextCave, current))
                         {
-                            var count = current.SmallCaveVisits.GetValueOrDefault(cave);
+                            var count = current.SmallCaveVisits.GetValueOrDefault(nextCave);
 
                             nextFrontier.Add(current with
                             {
-                                Path = current.Path.Add(next),
-                                SmallCaveVisits = current.SmallCaveVisits.SetItem(cave, count + 1)
+                                Path = current.Path.Add(nextCave),
+                                SmallCaveVisits = current.SmallCaveVisits.SetItem(nextCave, count + 1)
                             });
                         }
                     }
@@ -88,7 +93,7 @@ public class Challenge : ChallengeSync
                     {
                         nextFrontier.Add(current with
                         {
-                            Path = current.Path.Add(next)
+                            Path = current.Path.Add(nextCave)
                         });
                     }
                 }
@@ -104,56 +109,50 @@ public class Challenge : ChallengeSync
 
 internal static class ParseExtensions
 {
-    public static Graph ParseGraph(this IInputLines lines)
+    public static ImmutableDictionary<string, Cave> ParseGraph(this IInputLines lines)
     {
-        var links = new Dictionary<string, List<string>>();
+        var map = new Dictionary<string, List<string>>();
         foreach (var line in lines.AsString())
         {
-            var split = line.Split('-', StringSplitOptions.TrimEntries);
+            var split = line.Split('-');
+            var start = split[0];
+            var end = split[1];
 
-            AddEdge(links, split[0], split[1]);
+            map.TryAdd(start, new List<string>());
+            map.TryAdd(end, new List<string>());
+
+            map[start].Add(end);
+            map[end].Add(start);
         }
 
-        return new Graph(links);
-
-        static void AddEdge(Dictionary<string, List<string>> links, string to, string from)
-        {
-            if (links.TryGetValue(to, out var toEdges) is false)
+        return map
+            .ToImmutableDictionary(k => k.Key, kvp =>
             {
-                toEdges = links[to] = new List<string>();
-            }
+                var id = kvp.Key;
+                var caveType = id switch
+                {
+                    "start" => CaveType.Entrance,
+                    "end" => CaveType.Exit,
+                    // This is a little hack but based on our input data if the first char is lower then the rest is too
+                    _ when char.IsLower(id[0]) => CaveType.Small,
+                    _ => CaveType.Large,
+                };
 
-            if (links.TryGetValue(from, out var fromEdges) is false)
-            {
-                fromEdges = links[from] = new List<string>();
-            }
-
-            toEdges.Add(from);
-            fromEdges.Add(to);
-        }
+                return new Cave(id, caveType, kvp.Value.ToImmutableArray());
+            });
     }
 }
 
-class Graph
+readonly record struct Cave(string Id, CaveType CaveType, ImmutableArray<string> Connections);
+
+enum CaveType
 {
-    private readonly ImmutableDictionary<string, ImmutableArray<string>> _links;
-
-    public Graph(IDictionary<string, List<string>> links)
-    {
-        var nodes = ImmutableHashSet.CreateBuilder<string>();
-        nodes.UnionWith(links.Keys);
-        nodes.UnionWith(links.Values.SelectMany(v => v));
-
-        Nodes = nodes.ToImmutable();
-        _links = links.ToImmutableDictionary(k => k.Key, v => v.Value.ToImmutableArray());
-    }
-
-    public ImmutableHashSet<string> Nodes { get; }
-
-    public IEnumerable<string> GetEdges(string node)
-        => _links.TryGetValue(node, out var edges) ? edges : Enumerable.Empty<string>();
+    Entrance,
+    Exit,
+    Small,
+    Large,
 }
 
 record CaveRoute(
-    ImmutableArray<string> Path,
-    ImmutableDictionary<char, int> SmallCaveVisits);
+    ImmutableArray<Cave> Path,
+    ImmutableDictionary<Cave, int> SmallCaveVisits);
