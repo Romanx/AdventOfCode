09d432fb422a5cc9de8e6c72b10ede8e3e7805a3
Alex McAuliffe <alex.mcauliffe@willistowers.com>
2022-08-25 11:25:08 +0100

    Make 2019 day one challenge name standard

---
+++ src/years/2019/day-one/Challenge.cs
@@ -0,0 +1,42 @@
+﻿namespace DayOne2019
+{
+    public class Challenge : Shared.Challenge
+    {
+        public override ChallengeInfo Info { get; } = new ChallengeInfo(new LocalDate(2019, 12, 01), "The Tyranny of the Rocket Equation");
+
+        public void PartOne(IInput input, IOutput output)
+        {
+            var totalMass = input
+                .AsInts()
+                .Select(CalculateFuel)
+                .Sum();
+
+            output.WriteProperty("Total Mass", $"{totalMass:N0}");
+        }
+
+        public void PartTwo(IInput input, IOutput output)
+        {
+            var totalMass = input
+                .AsInts()
+                .Select(AggregateFuel)
+                .Sum();
+
+            output.WriteProperty("Total Mass", $"{totalMass:N0}");
+        }
+
+        private int CalculateFuel(int mass) => ((int)Math.Floor(mass / 3m)) - 2;
+
+        private long AggregateFuel(int mass)
+        {
+            var total = CalculateFuel(mass);
+            return total <= 0
+                ? 0L
+                : total + AggregateFuel(total);
+        }
+    }
+
+    internal static class InputExtensions
+    {
+        public static IEnumerable<int> AsInts(this IInput input) => input.Lines.AsMemory().Select(mem => int.Parse(mem.Span));
+    }
+}
