bdf5a071abf7cff9d5f0db5c36d6ae6f350dff02
Alex McAuliffe <alex.mcauliffe@willistowers.com>
2022-09-05 12:04:44 +0100

    2019 Day 9: Sensor Boost

---
+++ src/years/2019/day-nine/Challenge.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using Helpers;
+using Shared2019;
+
+namespace DayNine2019;
+
+public class Challenge : Shared.Challenge
+{
+    public override ChallengeInfo Info { get; } = new ChallengeInfo(new LocalDate(2019, 12, 9), "Sensor Boost");
+
+    public async Task PartOne(IInput input, IOutput output)
+    {
+        var program = input.AsIntcodeProgram();
+        var computer = new IntcodeComputer(program);
+        computer.Input.Writer.TryWrite(1);
+
+        var computerOutput = await computer.RunAndGetOutput();
+        var result = computerOutput[0];
+
+        output.WriteProperty("Computer result", result);
+    }
+
+    public async Task PartTwo(IInput input, IOutput output)
+    {
+        var program = input.AsIntcodeProgram();
+        var computer = new IntcodeComputer(program);
+        computer.Input.Writer.TryWrite(2);
+
+        var computerOutput = await computer.RunAndGetOutput();
+        var result = computerOutput[0];
+
+        output.WriteProperty("Computer result", result);
+    }
+}
