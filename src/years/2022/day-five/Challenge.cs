95b2afe648106d8f61916429279171d68a08f48e
Alex McAuliffe <alex.mcauliffe@willistowers.com>
2022-12-06 09:22:11 +0000

    2022 Day 5: Supply Stacks

---
+++ src/years/2022/day-five/Challenge.cs
@@ -0,0 +1,75 @@
+﻿using System.Text.RegularExpressions;
+
+namespace DayFive2022;
+
+public class Challenge : Shared.Challenge
+{
+    public override ChallengeInfo Info { get; } = new ChallengeInfo(new LocalDate(2022, 12, 5), "Supply Stacks");
+
+    public void PartOne(IInput input, IOutput output)
+    {
+        var (stacks, procedures) = input.Parse();
+
+        foreach (var procedure in procedures)
+        {
+            Apply(stacks, procedure);
+        }
+
+        output.WriteProperty("Top of Stacks", ReadTopOfStacks(stacks));
+
+        static void Apply(Stack<char>[] stacks, Procedure procedure)
+        {
+            var source = stacks[procedure.Source - 1];
+            var target = stacks[procedure.Target - 1];
+
+            for (var i = 0; i < procedure.Count; i++)
+            {
+                var item = source.Pop();
+                target.Push(item);
+            }
+        }
+    }
+
+    public void PartTwo(IInput input, IOutput output)
+    {
+        var (stacks, procedures) = input.Parse();
+
+        foreach (var procedure in procedures)
+        {
+            Apply(stacks, procedure);
+        }
+
+        output.WriteProperty("Top of Stacks", ReadTopOfStacks(stacks));
+
+        static void Apply(Stack<char>[] stacks, Procedure procedure)
+        {
+            var source = stacks[procedure.Source - 1];
+            var target = stacks[procedure.Target - 1];
+            var queue = new Stack<char>((int)procedure.Count);
+
+            for (var i = 0; i < procedure.Count; i++)
+            {
+                var item = source.Pop();
+                queue.Push(item);
+            }
+
+            while (queue.TryPop(out var item))
+            {
+                target.Push(item);
+            }
+        }
+    }
+
+    private string ReadTopOfStacks(Stack<char>[] stacks)
+    {
+        Span<char> result = stackalloc char[stacks.Length];
+        for (var i = 0; i < stacks.Length; i++)
+        {
+            result[i] = stacks[i].Peek();
+        }
+
+        return new string(result);
+    }
+}
+
+readonly record struct Procedure(uint Count, uint Source, uint Target);
---
+++ src/years/2022/day-five/ParseExtensions.cs
@@ -0,0 +1,64 @@
+﻿namespace DayFive2022;
+
+internal static class ParseExtensions
+{
+    private static readonly PcreRegex procedureRegex = new(@"move (\d+) from (\d+) to (\d+)");
+
+    public static (Stack<char>[] Stacks, ImmutableArray<Procedure> Procedures) Parse(this IInput input)
+    {
+        var paragraphs = input.Lines.AsParagraphs();
+
+        var stacks = ParseStacks(paragraphs[0]);
+        var procedures = ParseProcedures(paragraphs[1]);
+
+        return (stacks, procedures);
+    }
+
+    private static Stack<char>[] ParseStacks(ReadOnlyMemory<ReadOnlyMemory<char>> paragraph)
+    {
+        var span = paragraph.Span;
+
+        var stackCount = (int)double.Ceiling(span[0].Length / 4.0);
+
+        var stacks = new Stack<char>[stackCount];
+        for (var i = 0; i < stackCount; i++)
+        {
+            stacks[i] = new Stack<char>();
+        }
+
+        for (var idx = span.Length - 2; idx >= 0; idx--)
+        {
+            var start = 1;
+            var line = span[idx].Span;
+            for (var i = 0; i < stackCount; i++)
+            {
+                if (line[start] is not ' ')
+                {
+                    stacks[i].Push(line[start]);
+                }
+
+                start += 4;
+            }
+        }
+
+        return stacks;
+    }
+
+    private static ImmutableArray<Procedure> ParseProcedures(ReadOnlyMemory<ReadOnlyMemory<char>> readOnlyMemory)
+    {
+        var procedures = ImmutableArray.CreateBuilder<Procedure>(readOnlyMemory.Length);
+
+        for (var i = 0; i < readOnlyMemory.Length; i++)
+        {
+            var span = readOnlyMemory.Span[i].Span;
+            var match = procedureRegex.Match(span);
+            var count = uint.Parse(match.Groups[1]);
+            var source = uint.Parse(match.Groups[2]);
+            var target = uint.Parse(match.Groups[3]);
+
+            procedures.Add(new Procedure(count, source, target));
+        }
+
+        return procedures.MoveToImmutable();
+    }
+}
