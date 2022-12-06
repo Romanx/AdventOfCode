2832fe41ff13a27eee977f6f58291c584f1645ce
Alex McAuliffe <alex.mcauliffe@willistowers.com>
2022-12-06 10:43:46 +0000

    2022 Day 6: Tuning Trouble

---
+++ src/shared/Extensions/SpanExtensions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace System
+{
+    public static class SpanExtensions
+    {
+        public static bool IsDistinct(this ReadOnlySpan<char> input)
+        {
+            Span<int> counts = stackalloc int[256];
+
+            for (var i = 0; i < input.Length; i++)
+            {
+                var item = input[i];
+                ref var current = ref counts[item];
+                if (current is 1)
+                {
+                    return false;
+                }
+                current++;
+            }
+
+            return true;
+        }
+
+        public static ReadOnlySpan<char> Distinct(this ReadOnlySpan<char> input)
+        {
+            Span<int> counts = stackalloc int[256];
+
+            for (var i = 0; i < input.Length; i++)
+            {
+                var item = input[i];
+                counts[item]++;
+            }
+
+            Span<char> result = new char[input.Length];
+            var total = 0;
+            for (var i = 0; i < counts.Length; i++)
+            {
+                if (counts[i] is 1)
+                {
+                    result[total] = (char)counts[i];
+                    total++;
+                }
+            }
+
+            return result[..total];
+        }
+    }
+}
---
+++ src/years/2022/day-six/Challenge.cs
@@ -0,0 +1,48 @@
+﻿namespace DaySix2022;
+
+public class Challenge : Shared.Challenge
+{
+    public override ChallengeInfo Info { get; } = new ChallengeInfo(new LocalDate(2022, 12, 6), "Tuning Trouble");
+
+    public void PartOne(IInput input, IOutput output)
+    {
+        var span = input.Content.AsSpan();
+
+        const int markerLength = 4;
+
+        int markerIdx = -1;
+        for (var end = markerLength; end < span.Length; end++)
+        {
+            var start = end - markerLength;
+            var slice = span[start..end];
+            if (slice.IsDistinct())
+            {
+                markerIdx = end;
+                break;
+            }
+        }
+
+        output.WriteProperty("Start of Packet Marker Index", markerIdx);
+    }
+
+    public void PartTwo(IInput input, IOutput output)
+    {
+        var span = input.Content.AsSpan();
+
+        const int messageLength = 14;
+
+        int markerIdx = -1;
+        for (var end = messageLength; end < span.Length; end++)
+        {
+            var start = end - messageLength;
+            var slice = span[start..end];
+            if (slice.IsDistinct())
+            {
+                markerIdx = end;
+                break;
+            }
+        }
+
+        output.WriteProperty("Start of Message Marker Index", markerIdx);
+    }
+}
